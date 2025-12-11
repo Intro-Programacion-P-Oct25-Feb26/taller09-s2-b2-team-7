b879c812c92c2aca354a8fc73c5cb0bc423046f0
mesolano9 <mariaemiliasolano43@gmail.com>
2025-12-11 12:43:48 -0500

    ejercicio04

--- EjerciciosT9/src/paquete02/Ejercicio04.java
+++ EjerciciosT9/src/paquete02/Ejercicio04.java
@@ -15,15 +15,33 @@ public class Ejercicio04 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         double[] promedios = {10, 10, 9.1, 7, 6.1, 4, 8};
-        String[] estudiantes = {"Kimberly Gonzalez", "Mark Hogan", 
+        String[] estudiantes = {"Kimberly Gonzalez", "Mark Hogan",
             "Teresa Martinez", "Julia Johnson", "Mark Cook", "Jennifer Manning",
             "Juan Vasquez"};
         String[] promediosCualitativos = new String[7];
-        
-        
-        
-    }
 
+        for (int i = 0; i < promedios.length; i++) {
+            double promedio = promedios[i];
+            if (promedio >= 0 && promedio <= 5.9) {
+                promediosCualitativos[i] = "Promedio Regular";
+            } else {
+                if (promedio >= 6 && promedio <= 8.9) {
+                    promediosCualitativos[i] = "Promedio Bueno";
+                } else {
+                    if (promedio >= 9 && promedio <= 10) {
+                        promediosCualitativos[i] = "Promedio Sobresaliente";
+                    } else {
+                        promediosCualitativos[i] = "Nota Invalida";
+                    }
+
+                }
+            }
+        }
+        for (int i = 0; i < estudiantes.length; i++) {
+            System.out.printf("%s promedio: %.2f promedio cualitativo %s%n",
+                    estudiantes[i], promedios[i], promediosCualitativos[i]);
+        }
+    }
 }
