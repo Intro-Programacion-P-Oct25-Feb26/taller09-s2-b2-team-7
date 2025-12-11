1daf3ea7a1a40a0b657c29ea6cb69b7a2eae0541
jonathan <jonathanjavier2004@hotmail.com>
2025-12-11 12:04:25 -0500

    ejercicio05

--- EjerciciosT9/src/paquete02/Ejercicio05.java
+++ EjerciciosT9/src/paquete02/Ejercicio05.java
@@ -5,6 +5,7 @@
  */
 package paquete02;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 /**
@@ -17,7 +18,40 @@ public class Ejercicio05 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        String[] dias = new String[5];
+        int[] venta = new int[5];
+
+        for (int i = 0; i < dias.length; i++) {
+            switch (i) {
+                case 0:
+                    dias[i] = "Lunes";
+                    break;
+                case 1:
+                    dias[i] = "Martes";
+                    break;
+                case 2:
+                    dias[i] = "Miercoles";
+                    break;
+                case 3:
+                    dias[i] = "Jueves";
+                    break;
+                case 4:
+                    dias[i] = "Viernes";
+                    break;
+            }
+
+        }
+        for (int i = 0; i < dias.length; i++) {
+            System.out.printf("ingrese las ventas del dia %s\n", dias[i]);
+            int dato = entrada.nextInt();
+            venta[i] = dato;
+        }
+        for (int i = 0; i < dias.length; i++) {
+            System.out.printf("%s $%d\n", dias[i], venta[i]);
+        }
 
     }
 
