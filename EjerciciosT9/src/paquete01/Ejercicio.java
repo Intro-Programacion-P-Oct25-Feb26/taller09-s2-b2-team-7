c86c4dfc16c6abd2894ee621edf332771cb799cd
mesolano9 <mariaemiliasolano43@gmail.com>
2025-12-11 12:57:46 -0500

    ejercicio001

--- EjerciciosT9/src/paquete01/Ejercicio.java
+++ EjerciciosT9/src/paquete01/Ejercicio.java
@@ -4,10 +4,54 @@
  */
 package paquete01;
 
+import java.util.Scanner;
+import java.util.Locale;
+
 /**
  *
  * @author reroes
  */
 public class Ejercicio {
-    
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        String nombreMarca;
+        int total;
+        String[] listado;
+
+        System.out.println("¿Cuántas marcas desea ingresar?");
+        total = entrada.nextInt();
+        entrada.nextLine();
+
+        listado = new String[total];
+
+        for (int i = 0; i < listado.length; i++) {
+
+            System.out.println("Ingrese una marca:");
+            nombreMarca = entrada.nextLine();
+
+            String letra = nombreMarca.substring(0, 1);
+            
+            letra = letra.toLowerCase();
+
+            switch (letra) {
+                case "a":
+                case "c":
+                case "t":
+                    System.out.println("Marca no válida (empieza con A, C o T)."
+                            + " Intente otra.");
+                    i--;
+                    break;
+                default:
+                    listado[i] = nombreMarca;
+            }
+        }
+
+        for (int i = 0; i < listado.length; i++) {
+            System.out.printf("\nLa marca ingresada es:\nMarca: %S -> %d\n",
+                    listado[i], i);
+        }
+    }
 }
